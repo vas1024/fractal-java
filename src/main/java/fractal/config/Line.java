dfc04b40e182488a9a7131b71d1034cd43f5af72
i5nuc <ip14922@gmail.com>
2026-05-10 09:42:47 +0300

    nice

--- src/main/java/fractal/config/Line.java
+++ src/main/java/fractal/config/Line.java
@@ -1,4 +1,4 @@
 package fractal.config;
 
 
-public record Line(double x1, double y1, double x2, double y2) {}
+public record Line(double x1, double y1, double x2, double y2, double width ) {}
--- src/main/java/fractal/service/FractalService.java
+++ src/main/java/fractal/service/FractalService.java
@@ -17,7 +17,9 @@ public class FractalService {
         double x2 = x + (Math.cos(Math.toRadians(angle)) * length);
         double y2 = y + (Math.sin(Math.toRadians(angle)) * length);
 
-        lines.add(new Line(x, y, x2, y2));
+        double width = depth * 0.8;
+
+        lines.add(new Line(x, y, x2, y2, width));
 
         // Используем переданные параметры вместо жестко прописанных 25 и 0.75
         lines.addAll(generateTree(x2, y2, angle - angleStep, length * reduction, depth - 1, angleStep, reduction));
